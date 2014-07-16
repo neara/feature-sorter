632f1d071d11bad335fa893778b27a8c8f134f2e
neara <alkadur@gmail.com>
2014-07-16 19:15:46 +0300

    Created custom table view controller, with dynamic dataset. Created add feature view controller. Set navigation controller. Set relations between table view controller and add feature view controller.
    Signed-off-by: neara <alkadur@gmail.com>

---
+++ FeatureSorter/feature-sorter/SSDAddFeatureViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSDAddFeatureViewController.h
+//  FeatureSorter
+//
+//  Created by Ana Tr on 7/16/14.
+//  Copyright (c) 2014 SSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSDAddFeatureViewController : UIViewController
+
+@end
---
+++ FeatureSorter/feature-sorter/SSDFeaturesTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SSDFeaturesTableViewController.h
+//  FeatureSorter
+//
+//  Created by Ana Tr on 7/16/14.
+//  Copyright (c) 2014 SSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSDFeaturesTableViewController : UITableViewController
+
+// set cancel button from addFeatureView to redirect to FeaturesTableView
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
