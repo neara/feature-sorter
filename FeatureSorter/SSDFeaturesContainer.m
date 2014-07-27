6af9f4b57776c3b022c8d14b698f6c90bec5baa1
neara <alkadur@gmail.com>
2014-07-27 10:47:25 +0300

    Added features container. Updated single feature. Refs #7. Closes #2
    Signed-off-by: neara <alkadur@gmail.com>

---
+++ FeatureSorter/SSDFeaturesContainer.m
@@ -0,0 +1,22 @@
+//
+//  SSDFeaturesContainer.m
+//  FeatureSorter
+//
+//  Created by Ana Tr on 7/25/14.
+//  Copyright (c) 2014 SSD. All rights reserved.
+//
+
+#import "SSDFeaturesContainer.h"
+
+@interface SSDFeaturesContainer ()
+
+// used to hold all existing features
+@property (nonatomic, retain) NSMutableArray* items;
+
+@end
+
+@implementation SSDFeaturesContainer
+
+
+
+@end
