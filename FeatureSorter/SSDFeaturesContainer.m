6daea98b0ba58026053b42a5b6b8cf276e5428ce
neara <alkadur@gmail.com>
2014-12-06 12:44:13 +0200

    Emplemented get and remove methods
    Signed-off-by: neara <alkadur@gmail.com>

--- FeatureSorter/SSDFeaturesContainer.m
+++ FeatureSorter/SSDFeaturesContainer.m
@@ -17,6 +17,35 @@
 
 @implementation SSDFeaturesContainer
 
+/*
+ Get an item from current items
 
+ @param itemID
+ @returns: a pointer to the item
+*/
+-(SSDFeature*) get:(int32_t)itemID{
+    for (int i=0; i<[self.items count]; i++) {
+        SSDFeature * item = self.items[i];
+        if (item.ID == itemID) {
+            return item;
+        }
+    }
+    return NULL;
+};
+
+
+/*
+ Remove an item from current items
+ */
+-(BOOL) remove:(int32_t)itemID {
+    for (int i=0; i<[self.items count]; i++) {
+        SSDFeature * item = self.items[i];
+        if (item.ID == itemID) {
+            [self.items removeObjectAtIndex:i];
+            return YES;
+        }
+    }
+    return NO;
+};
 
 @end
